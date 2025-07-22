b341e9cefbb95c99c4e09b2f993a0dc66832ca79
TRJUNED5 <trjuned2@gmail.com>
2025-07-21 20:09:57 -0400

    "frontend"

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 import ImageModel from "./model/image_model.js"
 import multer from "multer";
 import dotenv from "dotenv"
+import cors from "cors"
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 dotenv.config()
 
