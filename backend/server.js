429dda5db528b77636463e533343aad86c32c80a
TRJUNED5 <trjuned2@gmail.com>
2025-07-22 22:26:18 -0400

    new

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.static("uploads"))
 dotenv.config()
 
 const client = new vision.ImageAnnotatorClient({
-  keyFilename: "healthy-hearth-466701-a5-4a7532f265a0.json",
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
 
 const storage = multer.diskStorage({
